a16ae902c39ae74ff489df210e1a30f943e2731e
apidullah <inceabdullah@yahoo.com>
2023-08-12 00:20:11 +0300

    add NEW_TUN_DEV

--- change_connection.sh
+++ change_connection.sh
@@ -9,4 +9,12 @@ info_log_await "Reading config...\n$CONFIG_FILE:"
 eval $(parse_yaml $CONFIG_FILE)
 cat $CONFIG_FILE
 
+OLD_TUN_DEV=$local_TUN_dev
+
+NEW_TUN_DEV=$(set_new_tun_dev "$OLD_TUN_DEV")
+if [ $? -eq 0 ]; then
+  echo "New tunnel device: $NEW_TUN_DEV"
+else
+  echo "An error occurred."
+fi
 
